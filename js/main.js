60a04a188319f6322f69ae2b9facf6157732a96d
Monica Zarate <monicazaratecortes@gmail.com>
2022-11-05 01:01:01 -0700

    Styled website. Style updates to js file.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@ const apiKey = "8SwsfpAgMhBu45ikVWZDANcFdUQJSXVyrwmhNjss";
 const apiURL = "https://api.nasa.gov/planetary/apod";
 
 let thisMonth = new Date().getMonth() + 1;
+let today = new Date().toISOString().slice(0, 10);
+
+let loading = document.querySelector("#loading");
 
 // Functions
 let getMonthPics = () => {
   // Add the spinner to the DOM
+  loading.classList.add("loading");
 
   fetch(
     `${apiURL}?api_key=${apiKey}&start_date=2022-${thisMonth}-01&thumbs=true`
@@ -19,9 +23,10 @@ let getMonthPics = () => {
     .catch((e) => {
       console.log(e);
     })
-    .finally
-    // Close the spinner
-    ();
+    .finally(
+      // Close the spinner
+      loading.classList.remove("loading")
+    );
 };
 
 let displayPictures = (data) => {
@@ -38,12 +43,12 @@ let displayPictures = (data) => {
   };
 
   if (Array.isArray(data)) {
+    thumbsDiv.innerHTML = "";
     let picsArr = data
       .map((pic) => {
         mediaType(pic);
-        return `<div><h3>Date:</h3>
-        <p>${pic.date}</p>
-        <img src="${imgUrl}" alt="" />
+        return `<div class="thumb"><div class="details"><h3>Date:</h3>
+        <p>${pic.date}</p></div><div class="details"><h3>Title:</h3><p>${pic.title}</p></div><img src="${imgUrl}" alt="" />
         <h3>Explanation</h3>
         <p>${pic.explanation}</p></div>`;
       })
@@ -54,9 +59,8 @@ let displayPictures = (data) => {
     thumbsDiv.innerHTML = "";
     let picObj = data;
     mediaType(data);
-    let picContainer = `<div><h3>Date:</h3>
-        <p>${picObj.date}</p>
-        <img src="${imgUrl}" alt="" />
+    let picContainer = `<div class="thumb"><div class="details"><h3>Date:</h3>
+        <p>${picObj.date}</p></div><div class="details"><h3>Title:</h3><p>${picObj.title}</p></div><img src="${imgUrl}" alt="" />
         <h3>Explanation</h3>
         <p>${picObj.explanation}</p></div>`;
     thumbsDiv.innerHTML = picContainer;
@@ -65,6 +69,8 @@ let displayPictures = (data) => {
 
 let selectedDate;
 let dateInput = document.querySelector("#date-input");
+dateInput.setAttribute("max", today);
+dateInput.setAttribute("value", today);
 
 let getSelectedDate = dateInput.addEventListener("change", () => {
   selectedDate = dateInput.value;
@@ -87,5 +93,8 @@ let getPicture = () => {
     ();
 };
 
-let requestBtn = document.querySelector("#btn");
-requestBtn.addEventListener("click", getPicture);
+let singleRequestBtn = document.querySelector("#single");
+singleRequestBtn.addEventListener("click", getPicture);
+
+let monthRequestBtn = document.querySelector("#month");
+monthRequestBtn.addEventListener("click", getMonthPics);
